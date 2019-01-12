1487d2a0b77ca9db7c1b8b92468209afe067d359
Aman <aman.mender@tegningtil3d.no>
2019-01-12 21:48:43 +0100

    Joke Library

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+Title: Random Joke Generator
+Descripton: Library that exports random jokes
+Author: Aman Mender
+Date: 12.01.19
+*/
+
+const jokes = require('./lib/jokes')
+const math = require('./lib/math')
+
+let intervall;
+if(process.argv.length > 2) {
+    intervall = parseInt(process.argv[3]);
+}
+
+const app = {};
+app.config = {
+    'jokeIntervall': intervall ? intervall : 1000
+}
+
+app.printJoke = () => {
+    const allJokes =  jokes.getAll();
+    const randomNumber = math.getRandomNumber(1,allJokes.length);
+    const selectedJoke = allJokes[randomNumber - 1];
+    console.log(selectedJoke)
+}
+
+app.indefiniteLoop = () => {
+    setInterval(app.printJoke,app.config.jokeIntervall)
+}
+
+app.indefiniteLoop();
